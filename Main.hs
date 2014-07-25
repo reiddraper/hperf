844bcb0d8f879374672130af90366a2f04ea9637
Reid Draper <reiddraper@gmail.com>
2014-07-25 09:52:35 -0500

    Try some basic exception handling for sendAll

--- Main.hs
+++ Main.hs
@@ -4,10 +4,14 @@ import Prelude hiding (null)
 
 import Data.ByteString (null, ByteString)
 
-import System.Environment (getArgs)
+import Control.Monad (void)
+import GHC.IO.Exception (IOErrorType(ResourceVanished))
 import System.Entropy (getEntropy)
-import qualified Network.Socket as S
+import System.Environment (getArgs)
+import System.IO.Error (catchIOError, ioeGetErrorType)
+
 import Network.Socket.ByteString (sendAll, recv)
+import qualified Network.Socket as S
 
 usage :: IO ()
 usage = putStrLn "Usage"
@@ -15,10 +19,12 @@ usage = putStrLn "Usage"
 message :: IO ByteString
 message = getEntropy 1500
 
-clientLoop :: S.Socket -> ByteString -> IO ()
-clientLoop sock msg = do
-    () <- sendAll sock msg
-    clientLoop sock msg
+clientLoop :: S.Socket -> ByteString -> IO Bool
+clientLoop sock msg = doSend >>= \b -> if b then clientLoop sock msg else return True
+    where doSend = catchIOError (sendAll sock msg >> return True) handleIOError
+          handleIOError e = if ioeGetErrorType e == ResourceVanished
+                               then return False
+                               else ioError e
 
 client :: IO ()
 client = do
@@ -26,7 +32,7 @@ client = do
     sock <- S.socket S.AF_INET S.Stream S.defaultProtocol
     host <- S.inet_addr "127.0.0.1"
     S.connect sock $ S.SockAddrInet 5001 host
-    clientLoop sock msg
+    void $ clientLoop sock msg
 
 serverLoop :: S.Socket -> IO ()
 serverLoop sock = do
