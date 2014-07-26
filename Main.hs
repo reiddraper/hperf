6e770e06887d89dfc10cd1ad60766b9952017a76
Reid Draper <reiddraper@gmail.com>
2014-07-26 14:13:19 -0500

    Playing around with some basic concurrent logging

--- Main.hs
+++ Main.hs
@@ -5,7 +5,9 @@ import Prelude hiding (null)
 import Data.ByteString (null, ByteString)
 
 import Control.Concurrent (forkIO)
-import Control.Monad (unless)
+import Control.Concurrent.STM (atomically)
+import qualified Control.Concurrent.STM.TBQueue as TBQueue
+import Control.Monad (unless, forever, void)
 import GHC.IO.Exception (IOErrorType(ResourceVanished))
 import System.Entropy (getEntropy)
 import System.Environment (getArgs)
@@ -13,6 +15,8 @@ import System.IO.Error (catchIOError, ioeGetErrorType)
 
 import Network.Socket.ByteString (sendAll, recv)
 import qualified Network.Socket as S
+
+data Message = NewClient | ClientDisconnected deriving (Show)
 
 usage :: IO ()
 usage = putStrLn "Usage"
@@ -36,16 +40,19 @@ client = do
     S.connect sock $ S.SockAddrInet 5001 host
     catchIOError (clientLoop sock msg) handleIOError
 
-serverThreadLoop :: S.Socket -> IO ()
-serverThreadLoop sock = do
+serverThreadLoop :: TBQueue.TBQueue Message -> S.Socket -> IO ()
+serverThreadLoop queue sock = do
     bytes <- recv sock 4096
-    unless (null bytes) $ serverThreadLoop sock
+    if null bytes
+       then atomically $ TBQueue.writeTBQueue queue ClientDisconnected
+       else serverThreadLoop queue sock
 
-serverLoop :: S.Socket -> IO ()
-serverLoop sock = do
+serverLoop :: TBQueue.TBQueue Message -> S.Socket -> IO ()
+serverLoop queue sock = do
     (clientSock, _clientAddr) <- S.accept sock
-    _threadID <- forkIO $ serverThreadLoop clientSock
-    serverLoop sock
+    _threadID <- forkIO $ serverThreadLoop queue clientSock
+    () <- atomically $ TBQueue.writeTBQueue queue NewClient
+    serverLoop queue sock
 
 server :: IO ()
 server = do
@@ -54,7 +61,14 @@ server = do
     host <- S.inet_addr "127.0.0.1"
     S.bind sock $ S.SockAddrInet 5001 host
     S.listen sock 1
-    serverLoop sock
+    queue <- atomically $ TBQueue.newTBQueue 16
+    _threadID <- logLoop queue
+    serverLoop queue sock
+
+logLoop :: TBQueue.TBQueue Message -> IO ()
+logLoop queue = void $ forkIO $ forever $ do
+    msg <- atomically $ TBQueue.readTBQueue queue
+    putStrLn $ "LOG: " ++ show msg
 
 main :: IO ()
 main = do
